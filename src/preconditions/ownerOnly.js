7fdbebf4ab8766f6bac33cc584775285b7bdd3ac
ItsThisTim <37299554+ItsThisTim@users.noreply.github.com>
2022-09-07 21:19:35 +0200

    expanded the ownerOnly precondition

--- src/preconditions/ownerOnly.js
+++ src/preconditions/ownerOnly.js
@@ -1,14 +1,25 @@
-import { Precondition } from "@sapphire/framework";
-const OWNERS = process.env.OWNERS.split(",");
+import { AllFlowsPrecondition } from '@sapphire/framework';
 
-export class ownerOnly extends Precondition {
-    isOwner(message, input) {
-        return input.includes(message.user.id)
+export class OwnerOnlyPrecondition extends AllFlowsPrecondition {
+    #message = 'This command can only be used by the owner.';
+
+    chatInputRun(interaction) {
+        return this.doOwnerCheck(interaction.user.id);
     }
 
-    async chatInputRun(interaction) {
-        return this.isOwner(interaction, OWNERS)
-            ? this.ok()
-            : this.error({ message: "Only the bot owner can use this command.", context: { silent: true } })
+    contextMenuRun(interaction) {
+        return this.doOwnerCheck(interaction.user.id);
+    }
+
+    messageRun(message) {
+        return this.doOwnerCheck(message.author.id);
+    }
+
+    /**
+     * Checks wether a user is a bot owner.
+     * @param {string} userId The user to check.
+     */
+    doOwnerCheck(userId) {
+        return process.env.OWNERS.split(',').includes(userId) ? this.ok() : this.error({ message: this.#message });
     }
 }
