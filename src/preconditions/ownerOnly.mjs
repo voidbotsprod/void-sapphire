f658495da8e280553cc1243b8457285a86941b79
Blue <lprwasunavailable@gmail.com>
2022-09-07 15:55:10 +0200

    removed check for normal cmds

--- src/preconditions/ownerOnly.mjs
+++ src/preconditions/ownerOnly.mjs
@@ -2,19 +2,12 @@ import { Precondition } from "@sapphire/framework";
 const OWNERS = process.env.OWNERS.split(",");
 
 export class ownerOnly extends Precondition {
-    isOwner(message, input, type) {
-        if (type == "messageRun") return input.includes(message.author.id)
-        if (type == "chatInputRun") return input.includes(message.user.id)
-    }
-
-    async messageRun(message) {
-        return this.isOwner(message, OWNERS, "messageRun")
-            ? await this.ok()
-            : this.error({ message: "Only the bot owner can use this command.", context: { silent: true } })
+    isOwner(message, input) {
+        return input.includes(message.user.id)
     }
 
     async chatInputRun(interaction) {
-        return this.isOwner(interaction, OWNERS, "chatInputRun")
+        return this.isOwner(interaction, OWNERS)
             ? this.ok()
             : this.error({ message: "Only the bot owner can use this command.", context: { silent: true } })
     }
