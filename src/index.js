21ff3fdab75225e235201450544fd03984c0668c
ItsThisTim <37299554+ItsThisTim@users.noreply.github.com>
2022-09-07 21:13:25 +0200

    more fancy ready.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,37 @@ import { container, LogLevel, SapphireClient, BucketScope } from '@sapphire/fram
 import { Time } from "@sapphire/time-utilities";
 
 const client = new SapphireClient({
-    intents: ['GUILDS', "GUILD_MESSAGES", "GUILD_MEMBERS"],
+    intents: [
+        'GUILDS',
+        'GUILD_MESSAGES',
+        'GUILD_MEMBERS'],
     defaultCooldown: {
         delay: Time.Second * 10,
         limit: 2,
-        filteredUsers: process.env.OWNERS.split(","),
+        filteredUsers: process.env.OWNERS.split(',',),
         scope: BucketScope.User
     },
     logger: { level: LogLevel.Info },
     allowedMentions: { repliedUser: true },
 });
 
+global.client = client;
+
 container.color = {
     "PASTEL_GREEN": "#87de7f",
     "CHERRY_RED": "#8e3741"
 }
 
-client.login();
+const main = async () => {
+    try {
+        client.logger.info('Logging in...');
+        await client.login(process.env.NODE_ENV == 'PRODUCTION' ? process.env.PROD_TOKEN : process.env.DEV_TOKEN);
+        client.logger.info(`Logged in as ${client.user.username} [${client.user.id}]`);
+    } catch (error) {
+        client.logger.fatal(error);
+        client.destroy();
+        process.exit(1);
+    }
+};
 
-global.client = client;
+main();
--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,20 +1,70 @@
-import { Listener } from "@sapphire/framework";
+import { Listener } from '@sapphire/framework';
+import { blue, gray, green, magenta, magentaBright, white, yellow } from 'colorette';
 
-export class Ready extends Listener {
-    constructor(context, options) {
+const dev = process.env.NODE_ENV === 'DEVELOPMENT';
+
+export class UserEvent extends Listener {
+    style = dev ? yellow : blue;
+
+    constructor(context, options = {}) {
         super(context, {
             ...options,
-            once: true,
-            event: "ready"
+            once: true
         });
     }
 
-    async run(client) {
-        const { username, id } = client.user;
+    run() {
+        this.printBanner();
+        this.printStoreDebugInformation();
+    }
 
-        console.log("----------------------")
-        this.container.logger.info(`Logged in as ${username} [${id}]`)
-        // vvv used to reset all slash commands if required
-        // await client.application.commands.set([])
+    
+    /**
+     * Prints a magenta (DEVELOPMENT) or blue (PRODUCTION) info banner depending on the NODE_ENV.
+    */
+    printBanner() {
+        const success = green('+');
+
+        const llc = dev ? magentaBright : white;
+        const blc = dev ? magenta : blue;
+
+        const line01 = llc('');
+        const line02 = llc('');
+        const line03 = llc('');
+
+        // Offset Pad
+        const pad = ' '.repeat(7);
+
+        console.log(
+            String.raw`
+${line01} ${pad}${blc(process.env.VERSION)}
+${line02} ${pad}[${success}] Gateway
+${line03}${dev ? ` ${pad}${blc('<')}${llc('/')}${blc('>')} ${llc(`${process.env.NODE_ENV} MODE`)}` : ''}
+		`.trim()
+        );
+    }
+
+    /**
+     * Prints the loaded stores.
+    */
+    printStoreDebugInformation() {
+        const { client, logger } = this.container;
+        let stores = [...client.stores.values()];
+        let first = stores.shift();
+        let last = stores.pop();
+
+        logger.info(this.styleStore(first, '┌─'));
+        for (const store of stores) logger.info(this.styleStore(store, '├─'));
+        logger.info(this.styleStore(last, '└─'));
+    }
+
+    /**
+     * Adds a symbol before a loaded store.
+     * @param {Store} store The store that got loaded.
+     * @param {string} prefix The symbol to show before the loaded store. 
+     * @returns {string} The styled string to print.
+     */
+    styleStore(store, prefix) {
+        return gray(`${prefix} Loaded ${this.style(store.size.toString().padEnd(3, ' '))} ${store.name}.`);
     }
 }
