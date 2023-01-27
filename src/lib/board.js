8a19cf00a50da17ee72230de58321ac29039a3e2
Blue <lprwasunavailable@gmail.com>
2023-01-27 23:53:58 +0100

    swapped canvas to new one

--- src/lib/board.js
+++ src/lib/board.js
@@ -1,5 +1,5 @@
 import { DB } from '#lib/functions';
-import { createCanvas, loadImage } from 'canvas';
+import { createCanvas, loadImage } from '@napi-rs/canvas';
 
 export default class Board {
 	constructor(id, type, guildId, name, description, sizeX, sizeY, createdAt, expireAt) {
